1082e11da62b15364b52ecfb9fbcdc8f8db3a2b7
Raymond-exe <Raymond-exe@users.noreply.github.com>
2020-02-29 10:58:37 -0800

    Created weapon.java
    See title

---
+++ weapon.java
@@ -0,0 +1,21 @@
+public class weapon {
+
+    private int durability;
+    private int range;
+    private int damage;
+
+    public weapon(int dr, int r, int da) {
+        durability = dr;
+        range = r;
+        damage = da;
+    }
+
+
+    /////GETTER METHODS\\\\\
+    public int GetDurability() { return durability; }
+    public int GetRange() { return range; }
+    public int GetDamage() { return damage; }
+
+    public void DecreaseDurability() { durability--; }
+
+}
