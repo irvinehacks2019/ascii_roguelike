df284760cbfd7cedf41d31336eca04545418bc85
dantecofano <61632094+dantecofano@users.noreply.github.com>
2020-02-29 11:57:01 -0800

    Enemy Class Created

---
+++ Enemy.java
@@ -0,0 +1,65 @@
+public class Enemy
+{
+  protected int hp;
+  protected int attack;
+  protected boolean isDead;
+  protected int xPos;
+  protected int yPos;
+  
+  public Enemy()
+  {
+    hp = 0;
+    attack = 0;
+    isDead = false;
+    xPos = 0;
+    yPos = 0;
+  }
+  
+  public int attack()
+  {
+   return attack; 
+  }
+  
+  public int getHP()
+  {
+   return hp; 
+  }
+  
+  public boolean checkDead()
+  {
+   return isDead; 
+  }
+  
+  public void takeDamage(int d)
+  {
+    if(isDead = true)
+    {
+      return;
+    }
+    hp = hp - d;
+    isDead = (hp <= 0);
+  }
+  
+  public void move(int dir)
+  {
+    if(dir == 1)
+    {
+      yPos++;
+    }
+    
+    if(dir == 2)
+    {
+     yPos--; 
+    }
+    
+    if(dir == 3)
+    {
+      xPos++;
+    }
+    
+    if(dir == 4)
+    {
+     xPos--; 
+    }
+  }
+}
