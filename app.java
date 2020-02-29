3384c72b38210b879c8a86a57f3622ecf998e83c
BlizardWizard <ivar369@gmail.com>
2020-02-29 12:03:15 -0800

    added *.class to gitignore

--- app.java
+++ app.java
@@ -1,6 +1,16 @@
+import java.util.Scanner
+
 public class app {
 
     pubic static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = userInput.nextLine();
+
+            System.out.println(input);
+        }
 
 
 
